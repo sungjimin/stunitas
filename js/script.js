b493b954ab57ebb02a798489b59e7f422d049942
sung-j <sunggggg0917@gmail.com>
2019-05-20 17:47:09 +0900

    icon color selected

--- js/script.js
+++ js/script.js
@@ -8,4 +8,8 @@ $(function(){
           $('*').removeClass('clicked');
           $(this).addClass('clicked');
       });
+    $('.menu>div').on('click', function(){
+            $('*').removeClass('selected');
+            $(this).addClass('selected');
+      });
 });
