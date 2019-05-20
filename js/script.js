b8063a8429c8b88a3a4bb15c47e55d9b76f557fa
sung-j <sunggggg0917@gmail.com>
2019-05-20 20:40:02 +0900

    css na부분 완료 script 아코디언nav html 마크업 완료

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
 // script.js
 $(function(){
+  //header 메뉴 클릭시 addClass.clicked(컬러 바뀌는 부분)입니다.
   $('ul.list>li').on('click', function(){
-        $('*').removeClass('clicked');
-        $(this).addClass('clicked');
+        $('*').removeClass('now');
+        $(this).addClass('now');
     });
-    $('nav>.menu').on('click', function(){
-          $('*').removeClass('clicked');
-          $(this).addClass('clicked');
+  $('nav>.menu').on('click', function(){
+          $('*').removeClass('clicked selected');
+          $(this).addClass('clicked').find('div').addClass('selected');
       });
-    $('.menu>div').on('click', function(){
-            $('*').removeClass('selected');
-            $(this).addClass('selected');
-      });
+  //header 메뉴 클릭시 addClass.clicked입니다//
+
+// section의 .nav 아코디언 효과
+var $gnb = $('div.nav .gnb');
+var $lnb = $('div.nav .close .lnb');
+
+$lnb.css('display', 'none');
+$gnb.click(function(){
+  var dp = $(this).next().css('display');
+  if(dp == 'none'){
+      $lnb.slideUp();
+      $(this).next().slideDown();
+    }else if(dp == 'block'){
+      $(this).next().slideUp();
+    }
+  });
+// section의 .nav 아코디언 효과//
 });
